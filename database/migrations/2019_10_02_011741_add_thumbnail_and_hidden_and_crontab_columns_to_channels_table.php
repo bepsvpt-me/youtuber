71c071645874d98200314edf533e08ef3cebb720
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2019-12-31 14:27:37 +0800

    add thumbnail, hidden and crontab columns to channels table

---
+++ database/migrations/2019_10_02_011741_add_thumbnail_and_hidden_and_crontab_columns_to_channels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddThumbnailAndHiddenAndCrontabColumnsToChannelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('channels', function (Blueprint $table) {
+            $table->text('thumbnail')->nullable()->after('comments');
+            $table->text('crontab')->nullable()->after('thumbnail');
+            $table->boolean('hidden')->index()->default(false)->after('crontab');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('channels', function (Blueprint $table) {
+            $table->dropColumn(['thumbnail', 'crontab', 'hidden']);
+        });
+    }
+}
