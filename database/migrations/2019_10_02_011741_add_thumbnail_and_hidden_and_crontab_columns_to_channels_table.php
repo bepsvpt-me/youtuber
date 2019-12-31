159e50b38bc623491aa0d194dd981f81423d9468
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2019-12-31 14:27:39 +0800

    do not add index to crontab column

--- database/migrations/2019_10_02_011741_add_thumbnail_and_hidden_and_crontab_columns_to_channels_table.php
+++ database/migrations/2019_10_02_011741_add_thumbnail_and_hidden_and_crontab_columns_to_channels_table.php
@@ -15,7 +15,7 @@ class AddThumbnailAndHiddenAndCrontabColumnsToChannelsTable extends Migration
     {
         Schema::table('channels', function (Blueprint $table) {
             $table->text('thumbnail')->nullable()->after('comments');
-            $table->text('crontab')->nullable()->index()->after('thumbnail');
+            $table->text('crontab')->nullable()->after('thumbnail');
             $table->boolean('hidden')->default(false)->index()->after('crontab');
         });
     }
