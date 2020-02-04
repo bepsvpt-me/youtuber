478e3b3b68cb8bc81c684affd52f5019b3372007
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-02-04 21:15:58 +0800

    register aes encryption helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,25 +3,39 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use phpseclib\Crypt\AES;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * Register services.
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
-        //
+        $this->app->singleton('aes', function ($app) {
+            $key = $app->config->get('app.key');
+
+            if (Str::startsWith($key, 'base64:')) {
+                $key = base64_decode(substr($key, 7));
+            }
+
+            $aes =  new AES();
+
+            $aes->setKey($key);
+
+            return $aes;
+        });
     }
 
     /**
-     * Bootstrap any application services.
+     * Bootstrap services.
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         //
     }
