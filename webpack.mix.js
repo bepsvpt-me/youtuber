5e0fbc2c6939896fae7fa825c08ed1635afa408a
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-02-07 23:52:11 +0800

    update mix webpack config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix');
 
+mix.disableNotifications();
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,5 +13,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/channel.js', 'public/js')
+  .js('resources/js/video.js', 'public/js')
+  .sass('resources/sass/app.scss', 'public/css')
+  .sourceMaps();
