6f0dc25dfb1394ac230f565b57a2f0c9f71d1f53
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-02-21 19:59:26 +0800

    update laravel-mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,23 +1,23 @@
 const mix = require('laravel-mix');
 
-mix.disableNotifications();
+require('laravel-mix-purgecss');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/channel.js', 'public/js')
+mix.disableNotifications()
+  .js('resources/js/channel.js', 'public/js')
   .js('resources/js/video.js', 'public/js')
   .sass('resources/sass/app.scss', 'public/css')
   .sourceMaps();
 
 if (mix.inProduction()) {
-  mix.version();
+  const options = {
+    postCss: [
+      require('postcss-discard-comments')({
+        removeAll: true
+      })
+    ],
+  };
+
+  mix.options(options)
+    .purgeCss()
+    .version();
 }
